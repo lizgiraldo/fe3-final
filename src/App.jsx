dcace2a300f60bc952da79460a2fc28f4971d81a
llizgiraldo <lizgiraldo15@gmail.com>
2024-09-13 11:10:58 -0500

    Add theme footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { ThemeProvider } from './Components/utils/global.context'; // Importa tu proveedor de tema
 import Home from './Routes/Home';
 import Contact from './Routes/Contact';
@@ -14,6 +14,10 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
+          {/* Redirige la ruta raíz a /Home */}
+          <Route path="/" element={<Navigate to="/Home" />} />
+          
+          {/* Define la ruta /Home */}
           <Route path="/Home" element={<Home />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/dentist/:id" element={<Detail />} />
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,21 +1,24 @@
-import React from 'react'
+import React, { useContext } from 'react'; // Asegúrate de importar useContext
+import { ThemeContext } from '../Components/utils/global.context'; // Importa tu contexto
 
 const Footer = () => {
+  const { theme } = useContext(ThemeContext); // Accede al tema actual
+
   return (
-    <footer>
+    <footer className={`footer ${theme === 'dark' ? 'footer-dark' : 'footer-light'}`}>
       <div className='footerInicial'>
         <p>Powered by</p>
         <img src="/images/DH.png" alt='DH-logo' />
       </div>
       <div className='footerIconos'>
-          <img src="/images/DH.png" alt='DH-logo' />
+          
           <img src="/images/ico-facebook.png"  alt="Facebook" />
           <img src="/images/ico-instagram.png"  alt="Instagram" />
           <img src="/images/ico-tiktok.png"  alt="Tiktok" />
           <img src="/images/ico-whatsapp.png"  alt="WhatsApp" />
       </div>
     </footer>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
